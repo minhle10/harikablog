701c905b0b526003e61706ae54e186f87d2686d0
minhle10 <lems@bc.edu>
2023-06-12 13:33:08 -0400

    creating home page and home routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "home#index"
+
+  get "/home", to: "home#index"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
