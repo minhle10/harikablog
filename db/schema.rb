a5f11852691c1980dd324e5cd314b14c961b9286
minhle10 <lems@bc.edu>
2023-06-13 00:16:31 -0400

    initialize db with post status and comment status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_034711) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_041427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_034711) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -29,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_034711) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   add_foreign_key "comments", "posts"
