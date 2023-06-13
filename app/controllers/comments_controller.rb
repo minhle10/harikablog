3ae80de11612dde78bae8856e93ede5891c1ea95
minhle10 <lems@bc.edu>
2023-06-13 01:13:21 -0400

    add destroy comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,16 @@
 class CommentsController < ApplicationController
     def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
-        redirect_to post_path(@post)
-      end
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.create(comment_params)
+      redirect_to post_path(@post)
+    end
+    
+    def destroy
+      @post = Post.find(params[:article_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to post_path(@post), status: :see_other
+    end
     
       private
         def comment_params
