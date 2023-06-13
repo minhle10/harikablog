7e103d0724fe4617205459727279ec6056d8ac4c
minhle10 <lems@bc.edu>
2023-06-13 00:28:20 -0400

    condense duplicated logic into a concern

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,9 @@
 class Post < ApplicationRecord
+    include Visible
+
     has_many :comments
 
     validates :title, presence: true
     validates :content, presence: true, length: { minimum: 10 }
 
-    VALID_STATUSES = ['public', 'private', 'archived']
-
-    validates :status, inclusion: { in: VALID_STATUSES }
-
-    def archived?
-        status == 'archived'
-    end
 end
