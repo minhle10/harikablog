a3c8ca3d909d47fd034a8bf06660fd92456a6606
minhle10 <lems@bc.edu>
2023-06-13 11:52:11 -0400

    redirect user to sign in page if user is null

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+    def authenticate_user!
+        redirect_to '/login' unless current_user
+    end
+
 
   # GET /posts or /posts.json
   def index
