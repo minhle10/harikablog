58dd18ab28c1db7af57b75c3bb741ec8769305cc
minhle10 <lems@bc.edu>
2023-06-13 01:59:54 -0400

    destroy associated comments on deletion of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     include Visible
 
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     validates :title, presence: true
     validates :content, presence: true, length: { minimum: 10 }
