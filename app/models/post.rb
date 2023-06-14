39af734b36dec95fbe277c07e7f53bf2fed81af9
minhle10 <lems@bc.edu>
2023-06-13 21:29:34 -0400

    resolve content display

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
     include Visible
-
+    has_rich_text :content
     has_many :comments, dependent: :destroy
 
     validates :title, presence: true
-    validates :content, presence: true, length: { minimum: 10 }
 
 end
