02a89799d39df700af578ca56a6b364414b74ec9
minhle10 <lems@bc.edu>
2023-06-19 11:30:05 -0400

    chaging routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
 
   get "/home", to: "home#index"
   get "/about", to: "about#index"
+  get "/chats", to: "chats#index"
   resources :posts do
     resources :comments
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :chats, only: %i[create show] do
+    resources :messages, only: %i[create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
