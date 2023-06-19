0f17c5780cf0d0e515991d1560b682df05d14091
minhle10 <lems@bc.edu>
2023-06-19 11:27:26 -0400

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_222525) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_051701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_222525) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "chats", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -60,6 +67,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_222525) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "chat_id", null: false
+    t.integer "role"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -99,5 +115,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_222525) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "chats", "users"
   add_foreign_key "comments", "posts"
+  add_foreign_key "messages", "chats"
 end
